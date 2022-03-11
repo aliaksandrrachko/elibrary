179ee3b789ad4a352d8bb0431585c66d82ce3555
aliaksandrrachko <rachko.a.ch@gmail.com>
2022-03-11 15:45:34 +0300

    add method with criteria api

--- elibrary-dao/src/main/java/by/it/academy/grodno/elibrary/dao/hibernate/BookDao.java
+++ elibrary-dao/src/main/java/by/it/academy/grodno/elibrary/dao/hibernate/BookDao.java
@@ -2,12 +2,19 @@ package by.it.academy.grodno.elibrary.dao.hibernate;
 
 import by.it.academy.grodno.elibrary.api.dao.hibernate.IBookDao;
 import by.it.academy.grodno.elibrary.entities.books.Book;
+import org.hibernate.query.criteria.internal.OrderImpl;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Order;
+import javax.persistence.criteria.Root;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BookDao extends AGenericDao<Book, Long> implements IBookDao {
 
@@ -40,6 +47,48 @@ public class BookDao extends AGenericDao<Book, Long> implements IBookDao {
                 .setFirstResult(calculateOffset(pageable.getPageNumber(), pageable.getPageSize()))
                 .setMaxResults(pageable.getPageSize()).getResultList();
         return new PageImpl<>(bookList, pageable, total);
+    }
+
+    public Page<Book> findAllBetween(LocalDateTime from, LocalDateTime to, Pageable pageable) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Book> cq = cb.createQuery(getGenericClass());
+
+        Root<Book> root = cq.from(getGenericClass());
+        CriteriaQuery<Book> select = cq.select(root);
+        addBetweenCreateDateCondition(from, to, cb, root, select);
+
+        CriteriaQuery<Long> countCq = cb.createQuery(Long.class);
+        Root<Book> countRoot = countCq.from(getGenericClass());
+        CriteriaQuery<Long> countSelect = countCq.select(cb.count(countRoot));
+        addBetweenCreateDateCondition(from,to, cb, countRoot, countSelect);
+        TypedQuery<Long> countTypedQuery = entityManager.createQuery(countSelect);
+        Long countResult = countTypedQuery.getSingleResult();
+
+
+        List<Order> orders = pageable.getSort().stream().map(order -> {
+            String property = order.getProperty();
+            boolean ascending = order.getDirection().isAscending();
+            return new OrderImpl(root.get(property), ascending);
+        }).collect(Collectors.toList());
+        select.orderBy(orders);
+
+        TypedQuery<Book> typedQuery = entityManager.createQuery(select)
+                .setFirstResult(calculateOffset(pageable.getPageNumber(), pageable.getPageSize()))
+                .setMaxResults(pageable.getPageSize());
+        List<Book> resultList = typedQuery.getResultList();
+
+        return new PageImpl<>(resultList, pageable, countResult);
+    }
+
+    private void addBetweenCreateDateCondition(LocalDateTime from, LocalDateTime to,
+                                               CriteriaBuilder cb, Root<Book> root, CriteriaQuery<?> select) {
+        if (from != null && to != null) {
+            select.where(cb.between(root.get("createDate"), from, to));
+        } else if (from != null) {
+            select.where(cb.greaterThan(root.get("createDate"), from));
+        } else if (to != null) {
+            select.where(cb.lessThan(root.get("createDate"), to));
+        }
     }
 
     private int calculateOffset(int page, int limit) {
